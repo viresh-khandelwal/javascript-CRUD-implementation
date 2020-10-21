055a9d2c13a119ada0b44d054b0f8d42a46da0be
Viresh khandelwal <vireshkhandelwal93@gmail.com>
2020-10-21 22:48:13 +0530

    displaying data on table and giving dynamic ids

--- data-service.js
+++ data-service.js
@@ -6,6 +6,17 @@ getData();
 async function getData(){
     await fetch("./data.json").then((data) => data.json()).then((data) => {
         console.log(data);
+        let tableBody = document.getElementById("playlist-body");
+        for(let song of data["favorite-songs"]){
+            console.log(song);
+            tableBody.innerHTML += "<tr>"+
+               "<td>"+song.id+"</td>"+
+               "<td>"+song.title+"</td>"+
+               "<td>"+song.singer+"</td>"+
+               "<td><button id='editButton"+song.id+"'>Edit</button></td>"+
+               "<td><button id='deleteButton"+song.id+"'>Delete</button></td>"
+            "</tr>";
+        }
     })
 }
 
