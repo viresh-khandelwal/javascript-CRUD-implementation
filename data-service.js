d9449af1737c3669069e1df6a9f424dc9554a2fb
Viresh khandelwal <vireshkhandelwal93@gmail.com>
2020-10-22 20:42:01 +0530

    update indexes and handle button clicks

--- data-service.js
+++ data-service.js
@@ -6,7 +6,7 @@ getData();
 async function getData(){
     await fetch("./data.json").then((data) => data.json()).then((data) => {
         console.log(data);
-        updateTable(data) ;   
+        updateTable(data["favorite-songs"]) ;   
     })
 }
 
@@ -14,20 +14,23 @@ function updateTable(data){
     tableData = data;
     let tableBody = document.getElementById("playlist-body");
     tableBody.innerHTML = "";
-    for(let song of tableData["favorite-songs"]){
+
+    for(let i in tableData){
+        let song = tableData[i];
+        let index = +i+1
         tableBody.innerHTML += "<tr>"+
-           "<td>"+song.id+"</td>"+
+           "<td>"+index+"</td>"+
            "<td>"+song.title+"</td>"+
            "<td>"+song.singer+"</td>"+
-           "<td><button id='editButton"+song.id+"'>Edit</button></td>"+
-           "<td><button id='deleteButton"+song.id+"'>Delete</button></td>"
+           "<td><button id='editButton"+song.id+"' onclick = 'openForm("+song.id+")'>Edit</button></td>"+
+           "<td><button id='deleteButton"+song.id+"' onclick = 'deleteSong("+song.id+")'>Delete</button></td>"
         "</tr>";
     }
 }
 
 function handleSearch(){
    let searchVal = event.target.value;
-   tableData["favorite-songs"] = tableData["favorite-songs"].filter((song) => {
+   tableData = tableData.filter((song) => {
             if(song.title.toLowerCase().indexOf(searchVal.toLowerCase()) !== -1){
                 return true;
             }else{
@@ -35,5 +38,13 @@ function handleSearch(){
             }
    });
    updateTable(tableData);
+}
+
+function openForm(id){
+    console.log(id)
+}
+
+function deleteSong(id){
+  console.log(id);
 }
 
